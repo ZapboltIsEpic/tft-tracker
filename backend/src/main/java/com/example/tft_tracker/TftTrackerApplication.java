df9c066b25d0ebc0298b27b498c6ddd5b90e8cae
YiJun Zou <zapboltisepic@gmail.com>
2025-12-29 17:39:08 +1100

    added simple spring boot backend

---
+++ backend/src/main/java/com/example/tft_tracker/TftTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tft_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/tft_tracker/TftTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tft_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
